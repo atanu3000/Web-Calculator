caa4527e2c84b7c16956b9da74b1b8f8948dd6fe
Atanu Paul <atanupaul2411@gmail.com>
2023-04-15 18:45:20 +0530

    update

--- calculate.js
+++ calculate.js
@@ -12,7 +12,12 @@ buttons.forEach((btn) => {
         // console.log(text.target.innerHTML);
         // console.log(nums)
         let ascii = text.target.value.charCodeAt();
-        if (text.target.innerHTML == 'C') {
+        if(text.target.innerHTML == '%')
+        {
+            inputs.innerHTML = `(${nums})%`;
+            nums = (nums/100);
+        }
+        else if (text.target.innerHTML == 'C') {
             nums = 0;
             inputs.innerHTML = nums;
         }
